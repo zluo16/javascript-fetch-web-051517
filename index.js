f0adf2e06b9411dad4241846b8ee85cfe1737ae0
zluo16 <zluo@alumni.berklee.edu>
2017-07-06 10:20:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const app = "I don't do much.";
+
+// let xhr = new XMLHttpRequest()
+// xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits')
+// xhr.responseType = 'json'
+//
+// xhr.onload = function() {
+//   console.log(xhr.response);
+// }
+//
+// xhr.onerror = function() {
+//   console.log('Boooo');
+// }
+//
+// xhr.send()
+
+const token = 'f904790dd57db7df23a1bfea356b56c04925444f'
+fetch('https://api.github.com/repos/jquery/jquery/commits', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
